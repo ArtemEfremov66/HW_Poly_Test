64e15b2c43329a64889a9ddcb06eaa37f66fd090
ArtemEfremov66 <artmasterae@mail.ru>
2024-07-10 00:17:31 +0300

    Add files via upload

---
+++ src/test/java/taxes/Tax6Test.java
@@ -0,0 +1,19 @@
+package taxes;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Tax6Test {
+    TaxSystem ts = new Tax6();
+    @Test
+    public void tax6Text() {
+        // given:
+        int debit = 1000;
+        int credit = 2000;
+        int expected = 60 ;
+        // when:
+        int result = ts.calcTaxFor(debit, credit);
+        // then:
+        Assertions.assertEquals(expected, result);
+    }
+}
